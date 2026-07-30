7c2d9db12bb5d2db1889d6bf245feab21ad85b74
GraziBrasileirosMundo <graziellesilvasoares@gmail.com>
2026-07-30 19:01:29 +0100

    Remove duplicate documentation files - keep only .md format

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
-const path = require('path');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: process.env.NEXT_DIST_DIR || '.next',
-  output: process.env.NEXT_OUTPUT_MODE,
   poweredByHeader: false,
   productionBrowserSourceMaps: false,
   eslint: {
@@ -13,13 +9,6 @@ const nextConfig = {
     ignoreBuildErrors: false,
   },
   images: { unoptimized: true },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.output.filename = 'static/chunks/[name]-[contenthash:8].js';
-      config.output.chunkFilename = 'static/chunks/[contenthash:16].js';
-    }
-    return config;
-  },
 };
 
 module.exports = nextConfig;
